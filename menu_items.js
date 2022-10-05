dc09b87c73e8ddb2755427a66af00c0bdedfdf54
patelsajan <patelsajan86@gmail.com>
2022-10-06 00:15:00 +0530

    some style changes

---
+++ menu_items.js
@@ -0,0 +1,9 @@
+var menu_items = {
+	1: {
+		'name': 'projects',
+		'child': {
+			'name': 'test_name',
+			'path':  './test.html'
+		}
+	}
+}
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let menu = document.getElementById('menu');
 let hamburger_icon = document.getElementById('hamburger');
 
-// actuate hamburger menu on click
+// actuate menu on click
 let isMenuOpen = false;
 hamburger_icon.addEventListener('click', event=> {
-	console.log('menu::', menu);
-	console.log('hamburger::', hamburger);
+	//console.log('menu::', menu);
+	//console.log('hamburger::', hamburger);
 	isMenuOpen = !isMenuOpen;
 	if(!isMenuOpen) {
 		menu.style.width = '0%';
@@ -13,3 +13,15 @@ hamburger_icon.addEventListener('click', event=> {
 		menu.style.width = '20%';
 	}
 });
+
+// populate menu items
+console.log('menu_items', menu_items);
+for(index in menu_items) {
+	let item = menu_items[index];
+	let div = document.createElement('div');
+	div.className = 'menu_item';
+	div.innerHTML = item['name'];
+	menu.appendChild(div);
+	if(item.hasOwnProperty('child')) {
+	}
+}
