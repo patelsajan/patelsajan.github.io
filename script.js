96a5d71829af7537635d542dc79fc58c5ae936e2
patelsajan <patelsajan86@gmail.com>
2023-05-06 22:25:17 +0530

    some changes in script

--- script.js
+++ script.js
@@ -18,14 +18,17 @@ for(index in menu_items) {
 	let item = menu_items[index];
 	let div = document.createElement('div');
 	div.className = 'menu_item';
-	div.innerHTML = item['name'];
+	//div.innerHTML = item['name'];
+	let button = document.createElement('button');
+	button.innerHTML = item.name;
+	div.appendChild(button);
 	if(item.hasOwnProperty('children')) {
 		console.log('children of', item.name, item['children']);
 		let child_div = document.createElement('div');
 		child_div.className = 'child_div  child_div_'+item.name;
 		child_div.id = 'child_div_' + item.name;
 		div.appendChild(child_div);
-		div.addEventListener('click', ev=> expand(item.name));
+		button.addEventListener('click', ev=> actuate_menu_item(item.name));
 		for(child_item of item['children']) {
 			let child_anchor = document.createElement('a');
 			child_anchor.innerHTML = child_item.name;
@@ -37,7 +40,7 @@ for(index in menu_items) {
 }
 
 //actuate menu items.
-function expand(item_name) {
+function actuate_menu_item(item_name) {
 	console.log('actuating menu item', item_name);
 	let item = document.getElementById(('child_div_'+item_name));
 	let height = item.offsetHeight;
